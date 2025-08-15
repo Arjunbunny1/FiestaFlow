cbc4ed01addd9974aa7e452e29a423d905f67a74
Arjunbunny1 <gargarjun907@gmail.com>
2025-08-15 20:25:59 +0530

    Render update

--- Backened/server.js
+++ Backened/server.js
@@ -12,7 +12,9 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: process.env.NODE_ENV === 'production' 
+    ? [process.env.FRONTEND_URL] 
+    : ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
